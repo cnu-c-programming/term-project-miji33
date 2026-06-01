653cf5f47936e0c32ef511a410e6383c164ef100
miji33 <kidmiji@gmail.com>
2026-06-01 20:36:27 +0900

    2차

---
+++ student.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <stdio.h>
+
+typedef struct Student {
+    int id;
+    char name[32];
+    int score;
+    struct Student* next;
+} Student;
+
+int add_stu(Student **head, int id, char *name, int score);
+int delete_stu(Student **head, int id);
+int update_stu(Student **head, int id, int score);
+Student *find_stu(Student *head, int id);
+void list_stu(Student *head);
+void stats_stu(Student *head);
+void exit_stu(Student *head);
